ed95959f287fa2f3354e3422ad150b3bb9c210fe
Eric Widhi Antara <nukenin.konoha@gmail.com>
2022-04-20 09:24:47 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <PostRequest msg="Post Request Using Vue.js" />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import PostRequest from "./components/PostRequest.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    PostRequest,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/PostRequest.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div class="card text-center m-3">
+      <h5 class="card-header">POST Request with Set Headers</h5>
+      <div class="card-body">Returned Id: {{ articleId }}</div>
+      <div class="card-body">Returned Message: {{ message }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "PostRequest",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      articleId: null,
+      message: null,
+    };
+  },
+  created() {
+    // POST request using axios with set headers
+    const body = { niu: "201701123", tahun: "2022", bulan: "04" };
+    const headers = {
+      Authorization:
+        "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6InNpYWJzZW5zaSIsImlhdCI6MTY1MDM0MjQyNSwiZXhwIjoxNjUwNDI4OTI1fQ.UPOk74EO4g1Iyp9hLGC-Dlbqj7vwOe2qcLpyU-g1JR4",
+      // "My-Custom-Header": "foobar"
+    };
+    axios
+      .post(
+        "https://absensi.undiksha.ac.id/services/absensi/history_presensi",
+        body,
+        { headers }
+      )
+      .then((response) => {
+        this.articleId = response.data.id;
+        this.message = response.data;
+      });
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
